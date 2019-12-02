27175938ea17ff215cbfef88b7c01f1843e92993
Yousra <yousra15b@correo.ugr.es>
2019-12-02 20:13:07 +0100

    ej8 y ej9 done

--- STL/ejercicio08.cpp
+++ STL/ejercicio08.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <list>
 using namespace std;
+template <typename T>
+list<T> listaInvertida(const list<T> &l) {
+  auto it = l.rbegin();
+  list<T> li;
+  for (it = l.rbegin(); it != l.rend(); ++it) {
+    li.push_back(*it);
+  }
+  return li;
+}
 int main(int argc, char const *argv[]) {
-  cout << "Holi 8" << endl;
+  list<int>::iterator it;
+  list<int> lista;
+  lista.push_back(1);
+  lista.push_back(2);
+  lista.push_back(3);
+  lista.push_back(4);
+  lista.push_back(5);
+  lista.push_back(6);
+  lista.push_back(7);
+  lista.push_back(8);
+  cout << "Antes de invertir: " << endl;
+  for (it = lista.begin(); it != lista.end(); ++it) {
+    cout << *it << endl;
+  }
+  cout << "******************" << endl;
+  cout << "Copia invertida: " << endl;
+  list<int> listaInv = listaInvertida(lista);
+  for (it = listaInv.begin(); it != listaInv.end(); ++it) {
+    cout << *it << endl;
+  }
   return 0;
 }
--- STL/ejercicio09.cpp
+++ STL/ejercicio09.cpp
@@ -2,10 +2,21 @@
 #include <list>
 #include <vector>
 using namespace std;
+template <typename T>
+void listaInvertida(list<T> &lista) {
+  int size = lista.size();
+  auto itFront = lista.begin();
+  auto itBack = lista.rbegin();
+  for (size_t i = 0; i < size / 2; i++) {
+    int aux = *itFront;
+    *itFront = *itBack;
+    *itBack = aux;
+    ++itFront;
+    ++itBack;
+  }
+}
 int main(int argc, char const *argv[]) {
-  cout << "Holi 9" << endl;
-  list<int>::iterator it, itFront;
-  list<int>::reverse_iterator itBack;
+  list<int>::iterator it;
   list<int> lista;
   lista.push_back(1);
   lista.push_back(2);
@@ -15,24 +26,13 @@ int main(int argc, char const *argv[]) {
   lista.push_back(6);
   lista.push_back(7);
   lista.push_back(8);
-
+  cout << "Antes de invertir: " << endl;
   for (it = lista.begin(); it != lista.end(); ++it) {
     cout << *it << endl;
   }
-  cout << "********************" << endl;
-
-  int size = lista.size();
-  itFront = lista.begin();
-  itBack = lista.rbegin();
-  // cout << *itBack << endl;
-  for (size_t i = 0; i < size / 2; i++) {
-    int aux = *itFront;
-    *itFront = *itBack;
-    *itBack = aux;
-    ++itFront;
-    ++itBack;
-  }
-
+  cout << "******************" << endl;
+  cout << "Despues de invertir: " << endl;
+  listaInvertida(lista);
   for (it = lista.begin(); it != lista.end(); ++it) {
     cout << *it << endl;
   }
