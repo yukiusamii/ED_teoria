8d6239a3eb9cc2ba12392cd600936816c4d4d43c
Yousra <yousra15b@correo.ugr.es>
2019-12-02 19:29:30 +0100

    ej3 y ej4 done

--- STL/ejercicio03.cpp
+++ STL/ejercicio03.cpp
@@ -1,6 +1,52 @@
+#include <cctype>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
+
+typedef map<string, int> dicc;
+
+string toLowerCase(const string cadena) {
+  string cad = cadena;
+  for (char &c : cad) {
+    c = tolower(c);
+  }
+
+  return cad;
+}
+
+dicc contadorPalabras(string nombreF) {
+  dicc cont;
+  ifstream f(nombreF);
+  if (!f) {
+    cout << "No existe el fichero " << nombreF << endl;
+  }
+  string palabra;
+  dicc::iterator it;
+  while (!f.eof()) {
+    f >> palabra;
+    palabra = toLowerCase(palabra);
+    it = cont.find(palabra);
+    if (it != cont.end()) {
+      (*it).second++;
+    } else {
+      cont[palabra] = 1;
+    }
+  }
+  return cont;
+}
+
 int main(int argc, char const *argv[]) {
-  cout << "Holi 3" << endl;
+  cout << "Escribe el nombre del fichero: " << endl;
+  string f;
+  getline(cin, f);
+  dicc cont;
+  cont = contadorPalabras(f);
+  dicc::iterator it;
+  for (it = cont.begin(); it != cont.end(); ++it) {
+    cout << (*it).first << ": " << (*it).second << endl;
+  }
+
   return 0;
 }
--- STL/ejercicio04.cpp
+++ STL/ejercicio04.cpp
@@ -1,6 +1,78 @@
+#include <cctype>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
+
+typedef map<string, int> dicc;
+typedef multimap<int, string> multiDicc;
+
+string toLowerCase(const string cadena) {
+  string cad = cadena;
+  for (char &c : cad) {
+    c = tolower(c);
+  }
+
+  return cad;
+}
+
+dicc contadorPalabras(string nombreF) {
+  dicc cont;
+  ifstream f(nombreF);
+  if (!f) {
+    cout << "No existe el fichero " << nombreF << endl;
+  }
+  string palabra;
+  dicc::iterator it;
+  while (!f.eof()) {
+    f >> palabra;
+    palabra = toLowerCase(palabra);
+    it = cont.find(palabra);
+    if (it != cont.end()) {
+      (*it).second++;
+    } else {
+      cont[palabra] = 1;
+    }
+  }
+  return cont;
+}
+
+multiDicc apareceXVeces(dicc cont) {
+  multiDicc inversCont;
+  dicc::iterator it;
+  for (it = cont.begin(); it != cont.end(); ++it) {
+    inversCont.insert(pair<int, string>((*it).second, (*it).first));
+  }
+  return inversCont;
+}
+
 int main(int argc, char const *argv[]) {
-  cout << "Holi 4" << endl;
+  cout << "Escribe el nombre del fichero: " << endl;
+  string f;
+  getline(cin, f);
+  dicc cont;
+  cont = contadorPalabras(f);
+  dicc::iterator it;
+  for (it = cont.begin(); it != cont.end(); ++it) {
+    cout << (*it).first << ": " << (*it).second << endl;
+  }
+
+  multiDicc inversCont;
+  inversCont = apareceXVeces(cont);
+  cout << "***************************" << endl;
+  cout << "Introduce un número: " << endl;
+  int n;
+  cin >> n;
+  multiDicc::iterator multIt;
+  multIt = inversCont.find(n);
+  cout << "Las plabras que aparecen " << n << " veces son: " << endl;
+  while (multIt != inversCont.end()) {
+    cout << (*multIt).second << endl;
+    ++multIt;
+    if ((*multIt).first != n) {
+      multIt = inversCont.end();
+    }
+  }
   return 0;
 }
